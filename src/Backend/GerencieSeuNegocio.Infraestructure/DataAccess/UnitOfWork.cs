1ecd596ad5224ba9316907e5883208df9466e84c
Pedro Vinicios <pedrooviniciossantos@gmail.com>
2025-08-01 20:02:35 -0300

    feat: add contract and implementation for UnitOfWork

---
+++ src/Backend/GerencieSeuNegocio.Infraestructure/DataAccess/UnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using GerencieSeuNegocio.Domain.Repositories;
+
+namespace GerencieSeuNegocio.Infraestructure.DataAccess
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly GerencieSeuNegocioDbContext _dbContext;
+
+        public UnitOfWork(GerencieSeuNegocioDbContext dbContext) => _dbContext = dbContext;
+
+        public async Task Commit() => await _dbContext.SaveChangesAsync();
+    }
+}
