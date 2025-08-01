8d9e95f48baac7e16366b36308930434136904e2
Pedro Vinicios <pedrooviniciossantos@gmail.com>
2025-08-01 19:47:01 -0300

    feat: add first version on register user feature

---
+++ src/Backend/GerencieSeuNegocio.Application/UseCases/User/Register/IRegisterUserUseCase.cs
@@ -0,0 +1,10 @@
+﻿using GerencieSeuNegocio.Communication.Requests.User.Register;
+using GerencieSeuNegocio.Communication.Responses.User.Register;
+
+namespace GerencieSeuNegocio.Application.UseCases.User.Register
+{
+    public interface IRegisterUserUseCase
+    {
+        public Task<ResponseRegisteredUserJson> Execute(RequestRegisterUserJson request);
+    }
+}
---
+++ src/Backend/GerencieSeuNegocio.Application/UseCases/User/Register/RegisterUserValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using GerencieSeuNegocio.Communication.Requests.User.Register;
+using GerencieSeuNegocio.Exceptions;
+
+namespace GerencieSeuNegocio.Application.UseCases.User.Register
+{
+    public class RegisterUserValidator : AbstractValidator<RequestRegisterUserJson>
+    {
+        public RegisterUserValidator()
+        {
+            RuleFor(user => user.Name).NotEmpty().WithMessage(ResourceMessagesException.NAME_EMPTY);
+
+            RuleFor(user => user.Email).NotEmpty().WithMessage(ResourceMessagesException.EMAIL_EMPTY);
+            RuleFor(user => user.Email).EmailAddress().WithMessage(ResourceMessagesException.EMAIL_INVALID);
+
+            RuleFor(user => user.Password.Length).GreaterThanOrEqualTo(6).WithMessage(ResourceMessagesException.PASSWORD_INVALID);
+        }
+    }
+}
