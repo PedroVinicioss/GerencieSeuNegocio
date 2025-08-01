3721c4347ac1f0f9373f9fc1d0da47fa8cfbb239
Pedro Vinicios <pedrooviniciossantos@gmail.com>
2025-08-01 19:43:18 -0300

    feat: add dependency injection for application/infra and automapper and cryptography

--- src/Backend/GerencieSeuNegocio.API/Program.cs
+++ src/Backend/GerencieSeuNegocio.API/Program.cs
@@ -13,6 +13,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddApplication(builder.Configuration);
+builder.Services.AddInfraestructure(builder.Configuration);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
