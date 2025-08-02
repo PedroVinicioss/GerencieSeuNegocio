e60177b2a861654cf79debecc4494cc7ed033b28
Pedro Vinicios <pedrooviniciossantos@gmail.com>
2025-08-01 21:36:24 -0300

    feat: adding database versions and user table

--- src/Backend/GerencieSeuNegocio.Infraestructure/DependencyInjectionExtension.cs
+++ src/Backend/GerencieSeuNegocio.Infraestructure/DependencyInjectionExtension.cs
@@ -1,4 +1,5 @@
-﻿using GerencieSeuNegocio.Domain.Repositories;
+﻿using FluentMigrator.Runner;
+using GerencieSeuNegocio.Domain.Repositories;
 using GerencieSeuNegocio.Domain.Repositories.User;
 using GerencieSeuNegocio.Infraestructure.DataAccess;
 using GerencieSeuNegocio.Infraestructure.DataAccess.Repositories;
@@ -6,6 +7,7 @@ using GerencieSeuNegocio.Infraestructure.Extensions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace GerencieSeuNegocio.Infraestructure
 {
@@ -14,6 +16,7 @@ namespace GerencieSeuNegocio.Infraestructure
         public static void AddInfraestructure(this IServiceCollection services, IConfiguration config)
         {
             AddDbContext(services, config);
+            AddFluentMigrator(services, config);
             AddRepositories(services);
         }
 
@@ -25,6 +28,19 @@ namespace GerencieSeuNegocio.Infraestructure
             services.AddDbContext<GerencieSeuNegocioDbContext>(dbContextOptions =>
             {
                 dbContextOptions.UseMySql(connectionString, serverVersion);
+            });
+        }
+        private static void AddFluentMigrator(IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.ConnectionString();
+
+            services.AddFluentMigratorCore().ConfigureRunner(options =>
+            {
+                options
+                    .AddMySql5()
+                    .WithGlobalConnectionString(connectionString)
+                    .ScanIn(Assembly.Load("GerencieSeuNegocio.Infraestructure"))
+                    .For.All();
             });
         }
 
