70c7ac2c10aeb9059d752534c5f2440b90709981
Pedro Vinicios <pedrooviniciossantos@gmail.com>
2025-08-04 16:16:44 -0300

    feat: endpoint put for update

--- src/Backend/GerencieSeuNegocio.API/Controllers/UserController.cs
+++ src/Backend/GerencieSeuNegocio.API/Controllers/UserController.cs
@@ -1,5 +1,8 @@
 ﻿using GerencieSeuNegocio.Application.UseCases.User.Register;
+using GerencieSeuNegocio.Application.UseCases.User.Update;
 using GerencieSeuNegocio.Communication.Requests.User.Register;
+using GerencieSeuNegocio.Communication.Requests.User.Update;
+using GerencieSeuNegocio.Communication.Responses;
 using GerencieSeuNegocio.Communication.Responses.User.Register;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,6 +19,17 @@ namespace GerencieSeuNegocio.API.Controllers
             var result = await useCase.Execute(request);
 
             return Created(string.Empty, result);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Update(
+            [FromServices] IUpdateUserUseCase useCase,
+            [FromBody] RequestUpdateUserJson request)
+        {
+            await useCase.Execute(request);
+            return NoContent();
         }
     }
 }
