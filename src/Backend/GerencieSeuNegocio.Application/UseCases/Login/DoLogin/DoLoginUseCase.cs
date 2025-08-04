4266d3362c478712fa57a6b4cfc96f98c3dccf5d
Pedro Vinicios <pedrooviniciossantos@gmail.com>
2025-08-04 20:26:47 -0300

    feat: adding generate token to usecase login

--- src/Backend/GerencieSeuNegocio.Application/UseCases/Login/DoLogin/DoLoginUseCase.cs
+++ src/Backend/GerencieSeuNegocio.Application/UseCases/Login/DoLogin/DoLoginUseCase.cs
@@ -1,7 +1,9 @@
 ﻿using GerencieSeuNegocio.Application.Services.Cryptography;
 using GerencieSeuNegocio.Communication.Requests.Login;
+using GerencieSeuNegocio.Communication.Responses.Token;
 using GerencieSeuNegocio.Communication.Responses.User.Create;
 using GerencieSeuNegocio.Domain.Repositories.User;
+using GerencieSeuNegocio.Domain.Security.Tokens;
 using GerencieSeuNegocio.Exceptions.ExceptionsBase;
 
 namespace GerencieSeuNegocio.Application.UseCases.Login.DoLogin
@@ -10,11 +12,16 @@ namespace GerencieSeuNegocio.Application.UseCases.Login.DoLogin
     {
         private readonly IUserReadOnlyRepository _userReadOnlyRepository;
         private readonly PasswordEncripter _passwordEncripter;
+        private readonly IAccessTokenGenerator _accessTokenGenerator;
 
-        public DoLoginUseCase(IUserReadOnlyRepository userReadOnlyRepository, PasswordEncripter passwordEncripter)
+        public DoLoginUseCase(
+            IUserReadOnlyRepository userReadOnlyRepository,
+            PasswordEncripter passwordEncripter,
+            IAccessTokenGenerator accessTokenGenerator)
         {
             _userReadOnlyRepository = userReadOnlyRepository;
             _passwordEncripter = passwordEncripter;
+            _accessTokenGenerator = accessTokenGenerator;
         }
 
         public async Task<ResponseCreateUserJson> Execute(RequestDoLoginJson request)
@@ -25,7 +32,11 @@ namespace GerencieSeuNegocio.Application.UseCases.Login.DoLogin
 
             return new ResponseCreateUserJson
             {
-                Name = user.Name
+                Name = user.Name,
+                Tokens = new ResponseTokensJson
+                {
+                    AccessToken = _accessTokenGenerator.Generate(user.Uuid, user.Role)
+                },
             };
         }
     }
---
+++ src/Backend/GerencieSeuNegocio.Domain/Security/Tokens/IAccessTokenGenerator.cs
@@ -0,0 +1,9 @@
+﻿using GerencieSeuNegocio.Domain.Enums;
+
+namespace GerencieSeuNegocio.Domain.Security.Tokens
+{
+    public interface IAccessTokenGenerator
+    {
+        public string Generate(Guid userIdentifier, RoleType role);    
+    }
+}
