9fd9758a89cf94b63da4c7d7458784978176d5b8
Pedro Vinicios <pedrooviniciossantos@gmail.com>
2025-08-04 16:44:18 -0300

    feat: adding a converter to eliminate white spaces

--- src/Backend/GerencieSeuNegocio.API/Program.cs
+++ src/Backend/GerencieSeuNegocio.API/Program.cs
@@ -4,13 +4,15 @@ using GerencieSeuNegocio.Application;
 using GerencieSeuNegocio.Infraestructure;
 using GerencieSeuNegocio.Infraestructure.Migrations;
 using GerencieSeuNegocio.Infraestructure.Extensions;
+using GerencieSeuNegocio.API.Converters;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)));
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new StringConverter()));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
