ecd14b1e19e3456a38a0065cd4fa334602b1347c
Pedro Vinicios <pedrooviniciossantos@gmail.com>
2025-08-04 23:22:02 -0300

    feat: add customizable attribute for authorization

---
+++ src/Backend/GerencieSeuNegocio.API/Attributes/AuthenticatedUserAttribute.cs
@@ -0,0 +1,12 @@
+﻿using GerencieSeuNegocio.API.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GerencieSeuNegocio.API.Attributes
+{
+    public class AuthenticatedUserAttribute : TypeFilterAttribute
+    {
+        public AuthenticatedUserAttribute() : base(typeof(AuthenticatedUserFilter))
+        {
+        }
+    }
+}
