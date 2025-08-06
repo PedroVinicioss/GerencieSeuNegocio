a08fa929cde8c979936f28597a8e20d3147b1e5f
Pedro Vinicios <pedrooviniciossantos@gmail.com>
2025-08-06 16:04:26 -0300

    feat: adding a GET endpoint and response for profile

--- src/Backend/GerencieSeuNegocio.API/Controllers/UserController.cs
+++ src/Backend/GerencieSeuNegocio.API/Controllers/UserController.cs
@@ -1,15 +1,28 @@
+using GerencieSeuNegocio.API.Attributes;
 using GerencieSeuNegocio.Application.UseCases.User.Create;
+using GerencieSeuNegocio.Application.UseCases.User.Profile;
 using GerencieSeuNegocio.Application.UseCases.User.Update;
 using GerencieSeuNegocio.Communication.Requests.User.Create;
 using GerencieSeuNegocio.Communication.Requests.User.Update;
 using GerencieSeuNegocio.Communication.Responses;
 using GerencieSeuNegocio.Communication.Responses.User.Create;
+using GerencieSeuNegocio.Communication.Responses.User.Profile;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GerencieSeuNegocio.API.Controllers
 {
     public class UserController : GerencieSeuNegocioBaseController
     {
+        [HttpGet]
+        [ProducesResponseType(typeof(ResponseUserProfileJson), StatusCodes.Status200OK)]
+        [AuthenticatedUser]
+        public async Task<IActionResult> GetUserProfile(
+        [FromServices] IGetUserProfileUseCase useCase)
+        {
+            var response = await useCase.Execute();
+            return Ok(response);
+        }
+
         [HttpPost]
         [ProducesResponseType(typeof(ResponseCreateUserJson), StatusCodes.Status201Created)]
         [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
@@ -25,6 +38,7 @@ namespace GerencieSeuNegocio.API.Controllers
         [HttpPut]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
+        [AuthenticatedUser]
         public async Task<IActionResult> Update(
             [FromServices] IUpdateUserUseCase useCase,
             [FromBody] RequestUpdateUserJson request)
---
+++ src/Shared/GerencieSeuNegocio.Communication/Responses/User/Profile/ResponseUserProfileJson.cs
@@ -0,0 +1,8 @@
+﻿namespace GerencieSeuNegocio.Communication.Responses.User.Profile
+{
+    public class ResponseUserProfileJson
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}
