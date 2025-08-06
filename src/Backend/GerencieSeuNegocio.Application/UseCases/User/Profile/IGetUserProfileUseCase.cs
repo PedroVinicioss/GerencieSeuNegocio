cee167636ea8167d325200b3d1feda90f3550c92
Pedro Vinicios <pedrooviniciossantos@gmail.com>
2025-08-06 16:05:37 -0300

    feat: adding a useCase for user profile

---
+++ src/Backend/GerencieSeuNegocio.Application/UseCases/User/Profile/IGetUserProfileUseCase.cs
@@ -0,0 +1,9 @@
+﻿using GerencieSeuNegocio.Communication.Responses.User.Profile;
+
+namespace GerencieSeuNegocio.Application.UseCases.User.Profile
+{
+    public interface IGetUserProfileUseCase
+    {
+        public Task<ResponseUserProfileJson> Execute();
+    }
+}
