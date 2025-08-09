9e414fa008eb2da073b40c3cd2ef6f7cf9588582
Pedro Vinicios <pedrooviniciossantos@gmail.com>
2025-08-08 22:37:48 -0300

    fix: fixing entity base long to int and fix updateuser usecase

--- src/Backend/GerencieSeuNegocio.Domain/Entities/EntityBase.cs
+++ src/Backend/GerencieSeuNegocio.Domain/Entities/EntityBase.cs
@@ -2,7 +2,7 @@
 {
     public class EntityBase
     {
-        public long Id { get; set; }
+        public int Id { get; set; }
         public Guid Uuid { get; set; } = Guid.NewGuid();
         public bool Active { get; set; } = true;
         public DateTime CreatOn { get; set; } = DateTime.UtcNow;
