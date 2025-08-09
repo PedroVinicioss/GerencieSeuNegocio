55205d478333125daf9f82344bcac18d2475dbcb
Pedro Vinicios <pedrooviniciossantos@gmail.com>
2025-08-08 22:38:52 -0300

    feat: adding new mapping to automapper

--- src/Backend/GerencieSeuNegocio.Application/Services/AutoMapper/AutoMapping.cs
+++ src/Backend/GerencieSeuNegocio.Application/Services/AutoMapper/AutoMapping.cs
@@ -1,8 +1,11 @@
 using AutoMapper;
+using GerencieSeuNegocio.Communication.Requests.Business.Create;
 using GerencieSeuNegocio.Communication.Requests.User.Create;
 using GerencieSeuNegocio.Communication.Requests.User.Update;
+using GerencieSeuNegocio.Communication.Responses.Business.Create;
 using GerencieSeuNegocio.Communication.Responses.User.Profile;
 using GerencieSeuNegocio.Domain.Entities;
+using GerencieSeuNegocio.Domain.Enums;
 
 namespace GerencieSeuNegocio.Application.Services.AutoMapper
 {
@@ -21,11 +24,19 @@ namespace GerencieSeuNegocio.Application.Services.AutoMapper
             CreateMap<RequestUpdateUserJson, User>()
                 .ForAllMembers(opt =>
                     opt.Condition((src, dest, srcMember) => srcMember != null));
+
+            CreateMap<RequestCreateBusinessJson, Business>()
+                .ForMember(d => d.Type, opt => opt.MapFrom(s =>
+                    Enum.Parse<BusinessType>(s.Type, true)));
         }
 
         private void DomainToResponse()
         {
             CreateMap<User, ResponseUserProfileJson>();
+
+            CreateMap<Business, ResponseCreateBusinessJson>()
+                .ForMember(dest => dest.Type, opt => opt.MapFrom(src =>
+                    Enum.GetName(typeof(BusinessType), src.Type)));
         }
     }
 }
