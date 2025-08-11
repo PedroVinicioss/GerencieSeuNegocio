79f3a1616b7866d002804d33254a5e28b99efaed
Pedro Vinicios <pedrooviniciossantos@gmail.com>
2025-08-11 19:30:15 -0300

    feat: adding request and response for create user

---
+++ src/Shared/GerencieSeuNegocio.Communication/Requests/Customer/Create/RequestCreateCustomerJson.cs
@@ -0,0 +1,11 @@
+﻿namespace GerencieSeuNegocio.Communication.Requests.Customer.Create
+{
+    public class RequestCreateCustomerJson
+    {
+        public int BusinessId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Document { get; set; } = string.Empty;
+    }
+}
