173167b29f78fff2145606a434de509013bb2665
Pedro Vinicios <pedrooviniciossantos@gmail.com>
2025-08-14 20:40:06 -0300

    feat: adding a version 07 for migration db referent a sales table

---
+++ src/Backend/GerencieSeuNegocio.Infraestructure/Migrations/Versions/Version0000007.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+
+namespace GerencieSeuNegocio.Infraestructure.Migrations.Versions
+{
+    [Migration(DatabaseVersions.TABLE_SALES, "Create table to save the sale's information.")]
+    public class Version0000007 : VersionBase
+    {
+        public override void Up()
+        {
+            CreateTable(SALES_TABLE_NAME)
+                .WithColumn("BusinessId").AsInt64().NotNullable().ForeignKey("FK_Sales_Business_Id", BUSINESS_TABLE_NAME, "Id")
+                .WithColumn("CustomerId").AsInt64().Nullable().ForeignKey("FK_Sales_Customers_Id", CUSTOMERS_TABLE_NAME, "Id")
+                .WithColumn("TotalAmount").AsDecimal().NotNullable().WithDefaultValue(0.0m)
+                .WithColumn("Status").AsInt32().NotNullable().WithDefaultValue(0)
+                .WithColumn("PaymentMethod").AsInt32().NotNullable().WithDefaultValue(0);
+        }
+    }
+}
