543a90437d73f9c9d1db1bf08eec8c08ee53d0f9
Pedro Vinicios <pedrooviniciossantos@gmail.com>
2025-08-13 21:59:05 -0300

    feat: adding a contracts and implementations for customer

---
+++ src/Backend/GerencieSeuNegocio.Infraestructure/DataAccess/Repositories/CustomerRepository.cs
@@ -0,0 +1,24 @@
+﻿using GerencieSeuNegocio.Domain.Entities;
+using GerencieSeuNegocio.Domain.Repositories.Customer;
+using Microsoft.EntityFrameworkCore;
+
+namespace GerencieSeuNegocio.Infraestructure.DataAccess.Repositories
+{
+    public class CustomerRepository : ICustomerReadOnlyRepository, ICustomerWriteOnlyRepository
+    {
+        private readonly GerencieSeuNegocioDbContext _dbContext;
+        public CustomerRepository(GerencieSeuNegocioDbContext dbContext) => _dbContext = dbContext;
+
+        public async Task Add(Customer customer, CancellationToken cancellationToken)
+        {
+            await _dbContext.Customers.AddAsync(customer, cancellationToken);
+        }
+
+        public async Task<Customer?> GetByDocument(string document, int businessId, CancellationToken cancellationToken = default)
+        {
+            return await _dbContext.Customers
+                .AsNoTracking()
+                .FirstOrDefaultAsync(c => c.Active && c.BusinessId == businessId && c.Document == document, cancellationToken);
+        }
+    }
+}
