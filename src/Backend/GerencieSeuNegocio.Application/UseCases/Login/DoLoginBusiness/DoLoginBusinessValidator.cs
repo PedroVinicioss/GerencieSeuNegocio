91bddc30b21ed754453a02d98724eda21e9d4521
Pedro Vinicios <pedrooviniciossantos@gmail.com>
2025-08-13 21:53:11 -0300

    feat: adding a new usecase for login a business

---
+++ src/Backend/GerencieSeuNegocio.Application/UseCases/Login/DoLoginBusiness/DoLoginBusinessValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using GerencieSeuNegocio.Communication.Requests.Login.DoLoginBusiness;
+using GerencieSeuNegocio.Exceptions;
+
+namespace GerencieSeuNegocio.Application.UseCases.Login.DoLoginBusiness
+{
+    public class DoLoginBusinessValidator : AbstractValidator<RequestDoLoginBusinessJson>
+    {
+        public DoLoginBusinessValidator()
+        {
+            RuleFor(x => x.BusinessUuid)
+                .NotEmpty()
+                .WithMessage(ResourceMessagesException.BUSINESS_EMPTY);
+        }
+    }
+}
