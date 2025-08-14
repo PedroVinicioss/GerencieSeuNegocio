c38cab7442bc3f8b53a455acf5666bc9088a00cf
Pedro Vinicios <pedrooviniciossantos@gmail.com>
2025-08-14 20:44:51 -0300

    feat: adding version 08 for migration db referent a sale items

---
+++ src/Backend/GerencieSeuNegocio.Infraestructure/Migrations/Versions/Version0000008.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+
+namespace GerencieSeuNegocio.Infraestructure.Migrations.Versions
+{
+    [Migration(DatabaseVersions.TABLE_SALE_ITEMS, "Create table to save the cash sale item's information.")]
+    public class Version0000008 : VersionBase
+    {
+        public override void Up()
+        {
+            CreateTable(SALE_ITEMS_TABLE_NAME)
+                .WithColumn("SaleId").AsInt64().NotNullable().ForeignKey("FK_SaleItems_Sales_Id", SALES_TABLE_NAME, "Id")
+                .WithColumn("ProductId").AsInt64().NotNullable().ForeignKey("FK_SaleItems_Products_Id", PRODUCTS_TABLE_NAME, "Id")
+                .WithColumn("Quantity").AsInt32().NotNullable().WithDefaultValue(1)
+                .WithColumn("UnitPrice").AsDecimal().NotNullable().WithDefaultValue(0.0m)
+                .WithColumn("TotalAmount").AsDecimal().NotNullable().WithDefaultValue(0.0m);
+        }
+    }
+}
