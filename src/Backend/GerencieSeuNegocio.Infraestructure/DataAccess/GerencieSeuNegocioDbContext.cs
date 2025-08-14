253e8ed4cd4de22940fa1632e4fe656a768b4b55
Pedro Vinicios <pedrooviniciossantos@gmail.com>
2025-08-14 20:26:40 -0300

    feat: adding a reference a Room table and next tables

--- src/Backend/GerencieSeuNegocio.Infraestructure/DataAccess/GerencieSeuNegocioDbContext.cs
+++ src/Backend/GerencieSeuNegocio.Infraestructure/DataAccess/GerencieSeuNegocioDbContext.cs
@@ -10,6 +10,13 @@ namespace GerencieSeuNegocio.Infraestructure.DataAccess
         public DbSet<User> Users { get; set; }
         public DbSet<Business> Business { get; set; }
         public DbSet<Customer> Customers { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<CashReport> CashReports { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Sales> Sales { get; set; }
+        public DbSet<SaleItems> SaleItems { get; set; }
+        public DbSet<Stay> Stays { get; set; }
+        public DbSet<StayCustomer> StayCustomers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
--- src/Backend/GerencieSeuNegocio.Infraestructure/Migrations/DatabaseVersions.cs
+++ src/Backend/GerencieSeuNegocio.Infraestructure/Migrations/DatabaseVersions.cs
@@ -5,5 +5,12 @@
         public const int TABLE_USER = 1;
         public const int TABLE_BUSINESS = 2;
         public const int TABLE_CUSTOMERS = 3;
+        public const int TABLE_ROOMS = 4;
+        public const int TABLE_CASH_REPORTS = 5;
+        public const int TABLE_PRODUCTS = 6;
+        public const int TABLE_SALES = 7;
+        public const int TABLE_SALE_ITEMS = 8;
+        public const int TABLE_STAYS = 9;
+        public const int TABLE_STAY_CUSTOMERS = 10;
     }
 }
